655ffbbb4d005a0adee02c8a4a5b477819b14bf5
McCrea Cobb <mccrea_cobb@fws.gov>
2022-02-23 16:04:24 -0900

    First package build

--- R/servcatr.R
+++ R/servcatr.R
@@ -1,47 +1,37 @@
-#' Query ServCat using the public REST Api
+#' Query ServCat reference profiles
 #'
 #' @param references a vector of numeric reference codes for ServCat records
 #'
 #' @return a list containing the contents of the parsed ServCat ari request and the api response
+#'
+#' @import httr
+#' @import jsonlite
+#'
 #' @export
 #'
 #' @examples
-#' dontrun{
-#' servcat_api(87544)
-#' servcat_api(c(87544, 87545))
+#' \dontrun{
+#' sc_profiles(87544)
+#' sc_profiles(c(87544, 87545))
 #' }
-servcat_api <- function(references = c(87544, 87545)){
-  library(httr)
-  library(jsonlite)
+sc_profiles <- function(references){
 
-  ### Send a request
-  ## Reference Profiles
-  #https://ecos.fws.gov/ServCatServices/servcat/v4/rest/Profile?q=87544%2C%2087545
-
-    comp_url <- "https://ecos.fws.gov/ServCatServices/servcat/v4/rest/Profile"
-    refs <- paste0(references, collapse = ", ")
-    query <- list(q = refs)
-    res <- GET(comp_url, query = query)
-
-  # if(quick_search) {
-  #   ## Quick search
-  #   comp_url <- "https://ecos.fws.gov/ServCatServices/servcat/v4/rest/ReferenceCodeSearch/Composite"
-  #   query <- list(q = 87544)
-  #   res <- GET(comp_url, query = query)
-  # }
+  # Send a request
+  comp_url <- "https://ecos.fws.gov/ServCatServices/servcat/v4/rest/Profile"
+  refs <- paste0(references, collapse = ", ")
+  query <- list(q = refs)
+  res <- httr::GET(comp_url, query = query)
 
   # Stop if the request doesn't return a json
-  if (http_type(res) != "application/json") {
-    stop("API did not return json", call. = FALSE)
+  if (httr::http_type(res) != "application/json") {
+    stop("ServCat API did not return json", call. = FALSE)
   }
 
+  # Parse the response
+  parsed <- jsonlite::fromJSON(rawToChar(res$content))
 
-  ### Parse the response
-  # Translate json to usable data frame
-  parsed <- fromJSON(rawToChar(res$content))
-
-  # Add error message if request fails
-  if (http_error(res)) {
+  # Stop message if request fails
+  if (httr::http_error(res)) {
     stop(
       sprintf(
         "ServCat API request failed [%s]\n%s",
@@ -52,6 +42,7 @@ servcat_api <- function(references = c(87544, 87545)){
     )
   }
 
+  # Return an S3 object
   structure(
     list(
       content = parsed,
@@ -60,6 +51,5 @@ servcat_api <- function(references = c(87544, 87545)){
     ),
     class = "servcat_api"
   )
-
 }
 
